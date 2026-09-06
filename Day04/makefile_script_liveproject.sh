ed198ec4a3fad442f3b31842037ed852a8b02156
𝐒ͱ꧊rêê𓂃 <atomixurshree@gmail.com>
2026-09-06 17:28:00 +0530

    Update makefile_script_liveproject.sh

--- Day04/makefile_script_liveproject.sh
+++ Day04/makefile_script_liveproject.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-DOCKER_COMPOSE: = docker-compose 
+
+DOCKER_COMPOSE := docker-compose
+
+OS := $(shell uname) 
+
+build:
+	
+USER_ID=$(id -u) GROUP_ID=$(id -g) $(DOCKER_COMPOSE) build	 
+
+up:   
+	 $(DOCKER_COMPOSE) up -d
+
+down:   
+	$(DOCKER_COMPOSE) down  
+
+
+clear:     
+	docker system prune -y
